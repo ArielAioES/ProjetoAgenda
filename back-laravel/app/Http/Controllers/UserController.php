275199d07dc9a7453c81f682d7e8969658873565
ArielAioES <ariel.aio@enterscience.com.br>
2024-04-15 16:43:14 -0300

    configurando cadastro/edição/deleção/listagem/listagem-única do usuário

--- back-laravel/app/Http/Controllers/UserController.php
+++ back-laravel/app/Http/Controllers/UserController.php
@@ -8,6 +8,30 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+        //Listar todos os usuários
+        public function index()
+    {
+        $users = User::all();
+
+        //Retornar os usuários cadastrados
+        return response()->json($users, 200);
+    }
+
+        //Listar um usuário específico
+        public function show($id){
+
+            $user = User::find($id);
+
+            //Validação caso não encontre o usuário específico
+            if(!$user){
+                return response()->json(["error" => "Usuário não encontrado"], 404);
+            }
+
+            //Retorna o usuário selecionado
+            return response()->json($user, 200);
+        } 
+
+        //Cadastrar um novo usuário
         public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -19,7 +43,8 @@ class UserController extends Controller
         // Verifica se o e-mail já está cadastrado
         $existingUser = User::where('email', $validatedData['email'])->first();
         if ($existingUser) {
-            return response()->json(["message" => "Usuário existente"], 400);
+            //Retorna mensagem de error, caso dê errado
+            return response()->json(["error" => "Usuário existente"], 400);
         }
 
         // Cria o usuário se o e-mail não estiver cadastrado
@@ -29,7 +54,46 @@ class UserController extends Controller
             "password" => bcrypt($validatedData["password"]),
         ]);
 
+        //Retorna mensagem de sucesso, caso seja cadastrado
         return response()->json(["message" => "Usuário cadastrado com sucesso"], 201);
     }
 
+        //Atualizar usuário cadastrado
+        public function update(Request $request, $id){
+
+            $validatedData = $request->validate([
+                "username" => "required|string|max:255",
+                "email" => "required|email",
+                "password" => "required|string|min:6",
+            ]);
+
+            // Verifica se o e-mail já está cadastrado
+            $existingUser = User::where('email', $validatedData['email'])->first();
+            if ($existingUser) {
+            //Retorna mensagem de error, caso dê errado
+            return response()->json(["error" => "Usuário existente"], 400);
+        }
+
+            $user = User::findOrFail($id);
+
+            $user->update([
+                "username"=> $validatedData["username"],
+                "email"=> $validatedData["email"],
+                "password"=> bcrypt($validatedData["password"]),
+            ]);
+
+            //Retorna mensagem de sucesso, caso seja cadastrado
+            return response()->json(["message"=> "Usuário atualizado com sucesso"],200);
+        }
+
+        public function destroy($id){
+            $user = User::find($id);
+
+            if (!$user) {
+                return response()-> json(["error"=> "Usuário não encontrado"],404);
+            }
+
+            $user->delete();
+            return response()->json(["message"=> "Usuário deletado com sucesso"],200);
+        }
 }
